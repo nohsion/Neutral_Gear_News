f831fdca4f1af35a70949f234dbcf16449f783b9
Sion Noh <shtldhs123@ajou.ac.kr>
2021-10-03 15:14:44 +0900

    selenium - 동적 컨텐츠 받아오기

--- backend/crawler_news.py
+++ backend/crawler_news.py
@@ -1,6 +1,8 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from selenium import webdriver
 
 client = MongoClient('localhost', 27017)
 db = client.dbnews
@@ -17,6 +19,12 @@ def crawler_daum_news(date):
     soup = BeautifulSoup(data.text, 'html.parser')
     news_list = soup.select('.list_news2 > li')
 
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+    driver = webdriver.Chrome(options=options)
+
     for news in news_list:
         rank = news.select_one('.rank_num.rank_popular > .wrap_num > .num_rank >.screen_out').text
         detail_url = news.select_one('.cont_thumb > .tit_thumb > a')['href']
@@ -28,9 +36,15 @@ def crawler_daum_news(date):
         info_news = news.select_one('.cont_thumb > .tit_thumb > .info_news').text
         detail_content = news.select_one('.cont_thumb > .desc_thumb > .link_txt').text.strip()
 
-        # soup2 = BeautifulSoup(requests.get(detail_url, headers=headers).text, 'html.parser')
-        # imoticons = soup2.select_one('.emotion_list')
-        # print(imoticons)
+        driver.get(detail_url)
+        time.sleep(0.5)  # 네트환경 환경에 따라...
+        soup2 = BeautifulSoup(driver.page_source, 'html.parser')
+        emoticon_list = soup2.select_one('.list-wrapper')
+        selects = emoticon_list.select('.count')
+
+        count_list = []
+        for i in range(len(selects)):
+            count_list.append(int(selects[i].text))
 
         doc = {
             'rank': rank,
@@ -38,10 +52,15 @@ def crawler_daum_news(date):
             'title': title,
             'detail_content': detail_content,
             'detail_url': detail_url,
-            'img_url': img_url
+            'img_url': img_url,
+            'nr_RECOMMEND': count_list[0],
+            'nr_LIKE': count_list[1],
+            'nr_IMPRESS': count_list[2],
+            'nr_ANGRY': count_list[3],
+            'nr_SAD': count_list[4],
         }
         db.headline.insert_one(doc)
-        print(rank, info_news, title, detail_content, detail_url, img_url)
+        print(rank, info_news, title, detail_content, detail_url, img_url, count_list)
 
 
 date = ''
